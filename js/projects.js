954e95768ef2ff50d0853cc45df92c21226f6bb1
Anneta Wamono <annetawamono@yahoo.com.au>
2021-01-16 13:37:33 -0800

    update: portfolio 2021

--- js/projects.js
+++ js/projects.js
@@ -1,14 +1,20 @@
 $(function() {
   $.getJSON("js/json/projects.json", function(data) {
     for (i in data.projects) {
-      var $newDiv = $("<div class='proj-item'></div>");
-      var $intOneDiv = $("<div class='col'></div>");
-      var $intTwoDiv = $("<div class='col'></div>");
-      var $img = $("<img>").attr( "src", data.projects[i].image);
+      let $newDiv = $("<div class='proj-item'></div>");
+      let $intOneDiv = $("<div class='col'></div>");
+      let $intTwoDiv = $("<div class='col'></div>");
+      let $img = $("<img>").attr( "src", data.projects[i].image);
+      let $stack = $("<p class='stack'></p>");
 
       $intOneDiv.append($img);
       $newDiv.append($intOneDiv);
 
+      for (j in data.projects[i].stack) {
+        $stack.append("<span>"+data.projects[i].stack[j]+"</span>");
+      }
+
+      $intTwoDiv.append($stack);
       $intTwoDiv.append("<h2><span>"+data.projects[i].name+"</span></h2>");
       $intTwoDiv.append("<p>"+data.projects[i].description+"</p>");
       if(data.projects[i].link != null) {
@@ -18,13 +24,6 @@ $(function() {
 
       $newDiv.append($intTwoDiv);
 
-      // $newDiv.append($img);
-      // $newDiv.append("<h2>"+data.projects[i].name+"</h2>");
-      // $newDiv.append("<p>"+data.projects[i].description+"</p>");
-      // if(data.projects[i].link != null) {
-      //   var $link = $("<a class='backHover' target='_blank'>View</a>").attr("href", data.projects[i].link);
-      //   $newDiv.append($link);
-      // }
       $("#project-container").append($newDiv);
     }
   });
